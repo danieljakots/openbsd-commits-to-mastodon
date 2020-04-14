bef3066e7e9b6ac77362067b60bf881a4a99cfe2
Daniel Jakots <danj@chown.me>
2020-04-14 19:01:08 -0400

    Enable mastodon

--- commits-to-mastodon.py
+++ commits-to-mastodon.py
@@ -24,8 +24,8 @@ TIME_BETWEEN_LOOPS = 600
 def awoo(account, message):
     _, _, access_token = get_credentials(account)
     print(account, access_token, message)
-    # mastodon = Mastodon(access_token=access_token, api_base_url=INSTANCE)
-    # mastodon.toot(message)
+    mastodon = Mastodon(access_token=access_token, api_base_url=INSTANCE)
+    mastodon.toot(message)
 
 
 def awooifneeded():
